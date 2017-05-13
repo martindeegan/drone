838f20a0af923dd7470f7c3e21bcbc155c5ff78c
Martin Deegan <mddeegan@gmail.com>
2017-05-13 12:28:01 -0400

    Added a timout

--- server.js
+++ server.js
@@ -4,15 +4,25 @@ var app = express();
 var drone_info = '';
 var desktop_info = '';
 
+//Information times out after a minute
+var drone_interval;
+var desktop_interval;
+
 app.get('/drone', function(req, res) {
   console.log('Requesting drone information');
   res.send(drone_info);
 });
 
 app.post('/drone', function(req, res) {
+  if(drone_interval) {
+    clearInterval(drone_interval);
+  }
   console.log('Posting drone information');
   drone_info = req.ip;
   console.log(drone_info);
+  drone_interval = setInterval(function() {
+    drone_info = '';
+  }, 60000);
   res.send('');
 });
 
@@ -22,9 +32,15 @@ app.get('/desktop', function(req, res) {
 });
 
 app.post('/desktop', function(req, res) {
+  if(desktop_interval) {
+    clearInterval(desktop_interval);
+  }
   console.log('Posting desktop information');
   desktop_info = req.ip;
   console.log(desktop_info);
+  desktop_interval = setInterval(function() {
+    desktop_info = '';
+  }, 60000);
   res.send('');
 });
 
