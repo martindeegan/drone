9550154c5046033b4e8f778a728a4963944100c4
Thomas Deegan <tadeegan@gmail.com>
2017-06-13 10:21:14 -0700

    add config

---
+++ src/config.rs
@@ -0,0 +1,29 @@
+
+
+use serde_json::{Value, Error};
+
+use std::vec::Vec;
+use std::fs::File;
+use std::io::prelude::*;
+use serde_json;
+
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Config {
+    pub kp: f32,
+    pub ki : f32,
+    pub kd : f32,
+    pub motors: Vec<u32>,
+    pub motor_cutoff: f32
+}
+
+impl Config {
+    pub fn new() -> Config {
+        let mut file = File::open("config.json").expect("failed to open config.json");
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).expect("failed to read config.json");
+        // Parse the string of data into serde_json::Value.
+        let v: Config = serde_json::from_str(&contents).expect("failed to desrialize json");
+        v
+    }
+}
