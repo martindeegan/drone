8dc98b2ba3d2a4740f0a8861ea487d2792fdb76e
Thomas Deegan <tadeegan@gmail.com>
2017-06-16 01:47:36 -0700

    smoother debugger

--- copter/debug/app.js
+++ copter/debug/app.js
@@ -1,88 +1,107 @@
-var xVal = 0;
-var yVal = 100;
+const RANGE = 50;
+const DOMAIN = 10000000; // 10 seconds.
 
-var maxData = 500;
-
-let p_data = [];
-let i_data = [];
-let d_data = [];
-let power_total = [];
-
-var options = {
-    scaleStepWidth: -1,
-    steppedLine: true,
+function insertAfter(newNode, referenceNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
 
-var chart = new CanvasJS.Chart("chartContainer", {
-    title: {
-        text: "PID + power"
-    },
-    data: [{
-        type: "line",
-        dataPoints: power_total,
-        color: "#F00",
-        options: options
-    }, {
-        type: "line",
-        dataPoints: p_data,
-        color: "#00F",
-        options: options
-    }, {
-        type: "line",
-        dataPoints: i_data,
-        options: options
-    }, {
-        type: "line",
-        dataPoints: d_data,
-        color: "#0F0",
-        options: options
-    }],
-    //  options: {
-    //        scales: {
-    //              yAxes: [{
-    //                  ticks: {
-    //                      stepSize: -1,
-    //                  }
-    //              }]
-    //          }
-    //  }
-});
+class Line {
+    constructor(canvas_id, color, name) {
+        var canvas = document.getElementById(canvas_id);
+        var ctx = canvas.getContext('2d');
 
-var refresh_rate = 4;
-var total_data = 0;
+        this.canvas = canvas;
+        this.ctx = ctx;
+        this.last_pt = null;
+        this.start_time = 0;
+        this.color = color;
+
+        let key = document.createElement("div");
+        key.innerText = name;
+        key.style.backgroundColor = color;
+        key.style.color = "White";
+        key.style.display = "block";
+        key.style.width = "100px";
+        insertAfter(key, canvas);
+    }
+
+    range(y) {
+        return (this.canvas.height / 2) + (y / RANGE * (this.canvas.height / 2))
+    }
+
+    domain(time) {
+        console.log("compute domain: " + time + "ms")
+        return (time - this.start_time) / DOMAIN * this.canvas.width
+    }
+
+    addData(time, power) {
+
+        if (!this.last_pt) {
+            this.last_pt = {
+                time: time,
+                power: power,
+            }
+            return;
+        }
+        let ctx = this.ctx;
+        ctx.beginPath();
+        ctx.moveTo(this.domain(this.last_pt.time), this.range(this.last_pt.power));
+        ctx.lineTo(this.domain(time), this.range(power));
+        ctx.strokeStyle = this.color;
+        ctx.lineWidth = 2;
+        ctx.stroke();
+        this.last_pt = {
+            time: time,
+            power: power,
+        }
+        if ((time - this.start_time) > DOMAIN) {
+            this.last_pt = null;
+            this.start_time = time;
+            this.clear()
+        }
+    }
+
+    clear() {
+        console.log("calling clear");
+        let ctx = this.ctx;
+        ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        ctx.lineWidth = 1;
+        // draw lines at intervals of 10
+        for (var i = -RANGE; i < RANGE; i += 10) {
+            ctx.beginPath();
+            ctx.moveTo(0, this.range(i));
+            console.log("this.canvas.width: " + this.canvas.width);
+            console.log("this.range(i): " + this.range(i));
+            ctx.lineTo(this.canvas.width, this.range(i));
+            ctx.strokeStyle = '#AAA';
+            ctx.stroke();
+        }
+
+        ctx.beginPath();
+        ctx.moveTo(0, this.range(0));
+        ctx.lineTo(this.canvas.width, this.range(0));
+        ctx.strokeStyle = 'black';
+        ctx.stroke();
+        this.last_pt = null;
+    }
+}
+
+var canvas = document.getElementById("x-axis");
+canvas.width = document.body.clientWidth;
+
+let totalline = new Line("x-axis", "red", "total");
+let pline = new Line("x-axis", "blue", "P");
+let iline = new Line("x-axis", "green", "I");
+let dline = new Line("x-axis", "orange", "D");
+
+pline.clear();
 
 function addData(t, total, p, i, d) {
-    p_data.push({
-        x: t,
-        y: p,
-    });
-    power_total.push({
-        x: t,
-        y: total,
-    });
-    i_data.push({
-        x: t,
-        y: i,
-    });
-    d_data.push({
-        x: t,
-        y: d,
-    });
-    truncate(p_data);
-    truncate(i_data);
-    truncate(d_data);
-    truncate(power_total);
-
-    if (total_data % refresh_rate == 0) {
-        console.log("render");
-        chart.render();
-    }
-}
-
-function truncate(arr) {
-    if (arr.length > maxData) {
-        arr.splice(0, 1);
-    }
+    pline.addData(t, p);
+    iline.addData(t, i);
+    dline.addData(t, d);
+    totalline.addData(t, total);
+    gotData = true;
 }
 
 var delegate = function(event) {
@@ -94,27 +113,36 @@ function setStatus(msg) {
     document.getElementById("connection-status").innerText = msg;
 }
 
+let gotData = false;
+
+function checkData() {
+    if (gotData) {
+        gotData = false;
+        setTimeout(checkData, 1000);
+    } else {
+        pline.clear();
+        dline.clear();
+        totalline.clear();
+        iline.clear();
+        reconnect();
+    }
+}
+
 function reconnect() {
     var socket = new WebSocket("ws://10.0.0.213:27070", "rust-websocket");
     socket.onerror = function() {
         setStatus("Connection closed... wating for connection.");
-        setTimeout( reconnect , 500);
+        setTimeout(reconnect, 500);
     }
     socket.close = function() {
         console.log("Connection closed");
         setStatus("Connection closed... wating for connection.");
-        setTimeout( reconnect , 500);
+        setTimeout(reconnect, 500);
     }
     socket.onopen = function() {
-         setStatus("Connected. Streaming data.");
-        // Modify array in place.
-        p_data.splice(0, p_data.length)
-        i_data.splice(0, p_data.length)
-        d_data.splice(0, p_data.length)
-        power_total.splice(0, p_data.length)
+        setStatus("Connected. Streaming data.");
+        setTimeout(checkData, 1000);
     }
-    
-    
     socket.onmessage = delegate;
 }
 
