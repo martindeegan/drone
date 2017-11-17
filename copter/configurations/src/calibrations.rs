ba3eed0375274a955d9618c983f7683923543cdf
Martin Deegan <martindeegan@Martins-MacBook-Pro.local>
2017-11-16 22:47:04 -0500

    Start of preparing for 1 minute flight test.

---
+++ copter/configurations/src/calibrations.rs
@@ -0,0 +1,7 @@
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Calibrations {}
+
+impl Calibrations {
+    pub fn new() -> Calibrations {}
+}
---
+++ copter/configurations/src/lib.rs
@@ -0,0 +1,7 @@
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate toml;
+
+mod config;
+mod calibrations;
